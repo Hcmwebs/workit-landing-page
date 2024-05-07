477770f30b10a3b48c7772a8eca618a6a34e6624
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-07 14:47:54 +1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='mx-auto'>
+			<h1 className='text-4xl font-bold text-center'>App goes here</h1>
+		</div>
+	);
+};
+
+export default App;
