55cc109fcda58710c775cd69e26992eb64334c97
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-09 11:05:01 +1200

    Updated the header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,9 @@ import { Hero, Navbar } from './index';
 
 const Header = () => {
 	return (
-		<header className=' w-full grid grid-cols-1 place-items-center'>
+		<header className=' w-full grid grid-cols-1 place-items-center bg-[#24053E]'>
 			<Navbar />
 			<Hero />
-			<button>learn</button>
 		</header>
 	);
 };
