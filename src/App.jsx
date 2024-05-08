0f6d1299003ed0c5ccf2cf74b8a021608c04c9f5
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-08 16:04:38 +1200

    set up the main app wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
 	return (
-		<div className='mx-auto'>
+		<div className='mx-auto w-full max-w-1440 py-4 md:py-10 lg:py-40'>
 			<h1 className='text-4xl font-bold text-center'>App goes here</h1>
 		</div>
 	);
