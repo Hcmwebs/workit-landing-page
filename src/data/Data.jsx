023b8f435ed46d2b84824072e1dc7cfe519baa31
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-09 16:53:51 +1200

    created the data file

---
+++ src/data/Data.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+const items = [
+	{
+		id: nanoid(),
+		number: 1,
+		title: 'Actionable insights',
+		text: 'Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics.',
+	},
+	{
+		id: nanoid(),
+		number: 2,
+		title: 'Data-driven decisions',
+		text: 'Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.',
+	},
+	{
+		id: nanoid(),
+		number: 3,
+		title: 'Always affordable',
+		text: 'Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.',
+	},
+];
+
+export default items;
