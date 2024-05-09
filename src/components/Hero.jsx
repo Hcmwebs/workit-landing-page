a4b18d4bac2ca95517ff7bcb0c34fd0587a68093
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-09 14:12:52 +1200

    optimized the images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,28 @@
+import heroImgWebp from '../assets/image-hero.webp';
+import heroImg from '../assets/image-hero.jpeg';
 const Hero = () => {
 	return (
-		<div className='hero min-h-screen bg-base-200'>
-			<div className='hero-content text-center'>
-				<div className='max-w-md'>
-					<h1 className='text-5xl font-bold'>Hello there</h1>
-					<p className='py-6'>
-						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-						a id nisi.
-					</p>
-					<button className='btn btn-primary'>Get Started</button>
+		<div className='hero align-element'>
+			<div className='w-full md:max-w-xl lg:max-w-[635px] hero-content text-center grid grid-cols-1 place-items-center gap-10'>
+				<h1 className='text-5xl md:text-6xl lg:text-7xl font-semibold text-white'>
+					Data {''}
+					<span className='underline underline-offset-8 decoration-[#44FFA1]'>
+						tailored
+					</span>{' '}
+					to your needs.
+				</h1>
+				<button className='btn btn-primary rounded-none border-0 bg-[#44FFA1] text-[#24053E] hover:border hover:border-[#44FFA1] hover:bg-[#24053E] hover:text-[#44FFA1] transition duration-500 ease-in-out cursor-pointer'>
+					Learn more
+				</button>
+				<div className='w-80 md:w-[514px] lg:w-[767px] h-[184px] md:h-[296px] lg:h-[441px]'>
+					<picture>
+						<source srcSet={heroImgWebp} type='image/webp' />
+						<img
+							src={heroImg}
+							alt='mobile phone'
+							className='w-full h-full object-cover'
+						/>
+					</picture>
 				</div>
 			</div>
 		</div>
