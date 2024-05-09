a2e3782cd89673d8358886fa2ac9bce8eee0a2c3
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-10 10:42:48 +1200

    added some block padding

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import SingleItem from './SingleItem';
 
 const MainContent = () => {
 	return (
-		<main className='w-full bg-[#FCF8FF] grid place-items-center py-16'>
+		<main className='w-full bg-[#FCF8FF] grid place-items-center py-20'>
 			<section className='w-full align-element grid grid-cols-1 lg:grid-cols-3 place-items-center gap-y-10 lg:gap-x-6'>
 				{items.map((item) => {
 					return <SingleItem key={item.id} {...item} />;
